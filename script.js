06b841cd0e4a06878b0d0db4fa172619d8299eeb
maxime.polese <maxime.polese@le-camus-numerique.fr>
2023-10-13 15:44:43 +0200

    index.html

---
+++ script.js
@@ -0,0 +1,20 @@
+let table = [1, 8, 56, 84, 62, 41, 12, 65, 99, 14, 33]
+
+function insertionSort(table){
+for (let el of table) {
+    el = table[el]
+    j = i
+}
+while j>0 and tableau[j-1]>en_cours:
+tableau[j]=tableau[j-1]
+j = j-1
+#on insère l'élément à sa place
+tableau[j]=en_cours
+
+}
+
+insertionSort(table);
+console.log(table);
+
+
+
