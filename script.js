dae1ac2a0c7c2bf2109aa7eb185c5f6b8cedad78
maxime.polese <maxime.polese@le-camus-numerique.fr>
2023-10-13 16:40:08 +0200

    insertion sort

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let table = [1, 8, 56, 84, 62, 41, 12, 65, 99, 14, 33]
+console.log(table);
 
-function insertionSort(table){
-for (let el of table) {
-    el = table[el]
-    j = i
-}
-while j>0 and tableau[j-1]>en_cours:
-tableau[j]=tableau[j-1]
-j = j-1
-#on insère l'élément à sa place
-tableau[j]=en_cours
-
+function insertionSort(table) {
+    let temp = 0;
+    let j = 0;
+    for (let i of table) {
+        console.log([i], i);
+        temp = table[i]
+        j = i -1
+        while (j > 0 && (table[j] > temp)) {
+            console.log('hello');
+            table[j] = table[j + 1]
+            j = j - 1
+        }
+        table[j +1] = temp
+    }
 }
 
 insertionSort(table);
